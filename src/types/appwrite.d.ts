27d1b8592f1ea7b42747a707d3fcbd566dd2c573
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-08-29 15:33:13 +0000

    fix duplicte enum implementation

--- src/types/appwrite.d.ts
+++ src/types/appwrite.d.ts
@@ -9,12 +9,6 @@ export enum Status {
     ARCHIVED = "archived"
 }
 
-export enum Status {
-    DRAFT = "draft",
-    PUBLISHED = "published",
-    ARCHIVED = "archived"
-}
-
 export enum TargetType {
     NOTE = "note",
     COMMENT = "comment"
@@ -76,22 +70,6 @@ export type ApiKeys = Models.Document & {
     lastUsedIp: string | null;
 }
 
-export type BlogPosts = Models.Document & {
-    id: string | null;
-    title: string | null;
-    content: string | null;
-    createdAt: string | null;
-    updatedAt: string | null;
-    authorId: string | null;
-    tags: string[] | null;
-    coverImage: string | null;
-    excerpt: string | null;
-    status: Status | null;
-    comments: string[] | null;
-    extensions: string[] | null;
-    metadata: string | null;
-}
-
 export type Comments = Models.Document & {
     noteId: string;
     userId: string;
@@ -143,14 +121,4 @@ export type Settings = Models.Document & {
     createdAt: string | null;
     updatedAt: string | null;
     mode: string | null;
-}
-
-export type AuthPasskeyChallenges = Models.Document & {
-    userId: string | null;
-    token: string | null;
-}
-
-export type AuthPasskeyCredentials = Models.Document & {
-    userId: string | null;
-    credentials: string | null;
 }
